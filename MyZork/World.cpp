e67e41c40a118d73515f077774517ab57604a146
miquelmiro3 <miquel.miro3@gmail.com>
2021-06-22 12:26:11 +0200

    README + details

--- MyZork/World.cpp
+++ MyZork/World.cpp
@@ -14,23 +14,23 @@ World::World() {
 	Room* _entrance = new Room("Entrance", "You are in a dark room illuminated by the hole in the ceiling from where you have fallen.");
 	Room* _forge = new Room("Forge", "You are in an abandoned forge surrouned by broken tools.");
 	Room* _darkRoom = new Room("DarkRoom", "You are in a small room.");
-	Room* _habitatedRoom = new Room("HabitatedRoom", "You are in a room where someone has lived for a long time.");
+	Room* _orcRoom = new Room("OrcRoom", "You are in a room where someone has lived for a long time.");
 	Room* _treasureChamber = new Room("TreasureChamber", "You are in a big room full of gold and treasures.");
 	
 	entities.push_back(_entrance);
 	entities.push_back(_forge);
 	entities.push_back(_darkRoom);
-	entities.push_back(_habitatedRoom);
+	entities.push_back(_orcRoom);
 	entities.push_back(_treasureChamber);
 
 	// CREATURES
-	Creature* _orc = new Creature("Orc", "A soldier orc, you may win him with a good weapon.", 20, 8, _habitatedRoom);
+	Creature* _orc = new Creature("Orc", "A soldier orc, you may win him with a good weapon.", 20, 8, _orcRoom);
 
 	// ITEMS
 	Item* _sword = new Item("Sword", "A standard sword.", equipable, _entrance);
 	Item* _magicRune = new Item("MagicRune", "A magic rune that can be attached to a sword.", upgrade, _forge);
 	Item* _ironKey = new Item("IronKey", "A iron key.", key, _darkRoom);
-	Item* _goldenKey = new Item("GoldenKey", "A golden key.", key, _habitatedRoom);
+	Item* _goldenKey = new Item("GoldenKey", "A golden key.", key, _orcRoom);
 
 	_goldenKey->Lock(_orc);
 	_sword->attack = 4;
@@ -39,7 +39,7 @@ World::World() {
 	// EXITS
 	Exit* _goldenDoor = new Exit("GoldenDoor", "You see a shiny golden door.", "east", "west", _entrance, _treasureChamber);
 	Exit* _ironDoor = new Exit("IronDoor", "You see a iron door.", "north", "south", _entrance, _forge);
-	Exit* _door = new Exit("Door", "You see a wooden door with no knob.", "south", "north", _darkRoom, _habitatedRoom);
+	Exit* _door = new Exit("Door", "You see a wooden door with no knob.", "south", "north", _darkRoom, _orcRoom);
 	Exit* _hole = new Exit("HoleOnTheWall", "You see a small hole but a person could crawl past.", "west", "east", _entrance, _darkRoom);
 
 	_goldenDoor->Lock(_goldenKey);
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # MyZork
+## Author
+I'm Miquel Miró and this is MyZork version of the original game Zork.
+## Commands
+Possible directions, you can use the full name or only the first letter:
+* north ( n )
+* west ( w )
+* south ( s )
+* east ( e )
+
+Commands, as rthe directions you can use the full name or only the first letter:
+* ( h ) help -> to get information on all possible commands
+* ( q ) quit -> to exit the game
+* ( l ) look -> to look the room
+* ( l ) look direction -> to look at a direction of the room
+* ( m ) move direction -> to move from a room to another
+* ( e ) examine -> to examine the room for entities
+* ( e ) examine entity -> to examine a item or a creature of the room or inventory (ex.: e Orc)
+* ( i ) inventory -> to see the items of your inventory
+* ( p ) pick item -> to pick an item of a room (ex.: p Sword)
+* ( d ) drop item -> to drop an item to a room
+* ( s ) stats -> to see the stats of your hero
+* ( eq ) equip item -> to equip an item of the inventory
+* ( a ) attack creature -> to attack a creature of a room
+* ( c ) combine item1 item2 -> to combine items of the inventory (you only can combine equipables with upgrades)
+
+## Guide
+The game consists in 5 rooms in the following disposition (X are empty):
+X F X
+D E T
+O X X
+
+E - Entrance: you can find a Sword to equip
+F - Forge: you can find a MagicRune to upgrade the Sword (IronKey needed)
+D - DarkRoom: you can find a IronKey
+O - OrcRoom: you can find a GoldenKey protected by an Orc
+T - TreasureChamber: you have to arrive here to win (GoldenKey needed)
+
+To win you need to arrive to the TreasureChamber, and for that you need the GoldenKey protected by the orc. The orc is pretty strong so to win him you need to equip an upgraded sword, you will find the Sword at the entrance and you can upgraded combining it with the MagicRune found in the Forge and for entering the Forge you need the IronKey found in the DarkRoom.
+
+## Links
+
+[Github](https://github.com/miquelmiro3/MyZork)
